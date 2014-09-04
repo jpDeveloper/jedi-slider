e85e3cdc6bd853963b3f03685da2e18c9d9e814f
jesus <jesus.parlange@gmail.com>
2014-09-04 13:59:59 -0700

    icons slider

--- js/scripts.js
+++ js/scripts.js
@@ -49,4 +49,28 @@ $(document).ready(function() {
 
   });
 
+  var owl = $("#ewoks");
+
+  owl.owlCarousel({
+      items : 10, //10 items above 1000px browser width
+      itemsDesktop : [992,7], //5 items between 1000px and 901px
+      itemsDesktopSmall : [768,5], // betweem 900px and 601px
+      itemsTablet: [600,3], //2 items between 600 and 0
+      itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+  });
+
+  // Custom Navigation Events
+  $(".next").click(function(){
+    owl.trigger('owl.next');
+  })
+  $(".prev").click(function(){
+    owl.trigger('owl.prev');
+  })
+  $(".play").click(function(){
+    owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
+  })
+  $(".stop").click(function(){
+    owl.trigger('owl.stop');
+  })
+
 });
