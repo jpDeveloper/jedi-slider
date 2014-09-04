d27950041666471c46652cb6ff4188e726655266
jesus <jesus.parlange@gmail.com>
2014-09-04 11:54:16 -0700

    The Force slider completed

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,16 @@ $(document).ready(function() {
 
   $("#owl-demo").owlCarousel({
 
-      navigation: true,
-       navigationText: [
-        "<i class='fa fa-angle-left fa-4x'></i>",
-        "<i class='fa fa-angle-right fa-4x'></i>"
-        ] // Show next and prev buttons
+      navigation : true,
+      navigationText: [
+       "<i class='fa fa-angle-left fa-4x'></i>",
+       "<i class='fa fa-angle-right fa-4x'></i>"
+       ], // Show next and prev buttons
       slideSpeed : 300,
       paginationSpeed : 400,
-      singleItem:true
+      singleItem:true,
+      pagination : false,
+      autoPlay : true,
 
       // "singleItem:true" is a shortcut for:
       // items : 1,
